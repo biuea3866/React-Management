de529ad64fd535cf8c4241c3f92483bf24df8a80
biuea3866 <biuea3866@gmail.com>
2020-07-08 20:34:00 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- management/server.js
+++ management/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+const data = fs.readFileSync('./database.json')
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database,
+    insecureAuth : conf.insecureAuth
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res)=>{
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': 'Hong gil-dong',
-            'birthday' : '961222',
-            'gender' : 'Male',
-            'jobs' : 'Graduent'
-        },
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': 'Kim gae-ddong',
-            'birthday' : '001201',
-            'gender' : 'Male',
-            'jobs' : 'Programmer'
-        },
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': 'Park chul-soo',
-            'birthday' : '930201',
-            'gender' : 'Male',
-            'jobs' : 'None'
-        }
-    ])
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        } 
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
