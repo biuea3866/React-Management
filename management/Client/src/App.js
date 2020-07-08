53bd39142c69bf4a03e559dad8f63565c542632a
biuea3866 <biuea3866@gmail.com>
2020-07-08 19:48:11 +0900

    Add Customer API Loading View

--- management/Client/src/App.js
+++ management/Client/src/App.js
@@ -8,6 +8,7 @@ import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
 import {withStyles} from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme =>({
   root:{
@@ -17,17 +18,37 @@ const styles = theme =>({
   },
   table:{
     minWidth: 1080
+  },
+  progress:{
+    margin : theme.spacing.unit * 2 
   }
 })
 
+/*
+  React Life Cycle
+
+  1) constructor()
+
+  2) componentWillMount()
+
+  3) render()
+
+  4) componentDidMount()
+*/
+
+/*
+  props or state => shouldComponentUpdate()
+*/
 // Body Tag
 class App extends Component{
   // 변경가능
   state={
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers : res}))
     .catch(err => console.log(err))
@@ -40,6 +61,10 @@ class App extends Component{
     return body;
   }
 
+  progress = () => {
+    const{completed} = this.state
+    this.setState({completed: completed >= 100 ? 0: completed + 1})
+  }
   render(){
     // props 변경불가
     const {classes} = this.props
@@ -69,7 +94,13 @@ class App extends Component{
                 birthday={c.birthday}
                 gender={c.gender}
                 jobs={c.jobs}/>)
-              }) : ""}
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+              }
           </TableBody>
         </Table>
       </Paper>
